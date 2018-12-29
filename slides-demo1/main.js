dbfac231d36fdcc1eb66c358522c7dba84beb66d
你的Jasmine <360925221@qq.com>
2018-12-29 22:08:49 +0800

    无缝轮播

---
+++ slides-demo1/main.js
@@ -0,0 +1,43 @@
+let n
+初始化()
+setInterval(() => {
+    makeLeave(getImage(n))
+.one('transitioned',(e)=>{
+    makeEnter($(e.currentTarget))
+})
+makeCurrent(getImage(n+1))
+n+=1
+}, 3000)
+
+
+
+
+
+
+function getImage(n){
+return $('.images>img:nth-child(${x(n)})')
+}
+
+function x(n){
+    if (n>6){
+        n=n%6
+        if(n===0){
+            n=6
+        }
+    }//n=1,2,3,4,5,6
+    return n
+}
+function 初始化(){
+    n = 1
+    $('.images > img:nth-child(${n})').addClass('current')
+      .siblings().addClass('enter')
+  }
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+    return $node.removeClass('current leave').addClass('enter')
+}
